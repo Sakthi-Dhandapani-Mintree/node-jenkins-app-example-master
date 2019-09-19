5bd41637d01a4b5689a6039e9249f7124b65eb54
Sakthi-Dhandapani-Mintree <43699383+Sakthi-Dhandapani-Mintree@users.noreply.github.com>
2019-09-19 13:14:12 +0530

    Update Customers.js

--- src/Customers.js
+++ src/Customers.js
@@ -41,7 +41,7 @@ export default class Customers extends Component {
               <p>{customer.phone}</p>
               <Button bsStyle="info" onClick={() => this.setState({selectedCustomer: customer.id})}>
 
-                Click to View Seetharaman Custormer Details
+                Click to View Seetharaman@Mindtree Custormer Details
 
               </Button>
 
