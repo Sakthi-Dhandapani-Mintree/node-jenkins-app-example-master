5eeccfc7120069e505b42095544c04745fe63236
Sakthi-Dhandapani-Mintree <43699383+Sakthi-Dhandapani-Mintree@users.noreply.github.com>
2019-09-27 19:44:33 +0530

    Create Welcome.js

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+class Welcome extends React.Component {
+    render() {
+        const myStyle = {color:'red'}
+        return (
+            <div><h1 style={myStyle}>
+                Welcome file called!!!</h1></div>
+        )
+    }
+}
+export default Welcome
